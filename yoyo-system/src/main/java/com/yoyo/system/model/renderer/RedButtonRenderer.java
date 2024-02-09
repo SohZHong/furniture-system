16c5905ebb06269de1a5da4426fedea0987a0035
Soh Zhe Hong <sohzhehong09@gmail.com>
2024-02-09 13:45:44 +0800

    Added Inheritance

--- yoyo-system/src/main/java/com/yoyo/system/model/renderer/RedButtonRenderer.java
+++ yoyo-system/src/main/java/com/yoyo/system/model/renderer/RedButtonRenderer.java
@@ -2,31 +2,23 @@ package com.yoyo.system.model.renderer;
 
 import java.awt.Color;
 import java.awt.Component;
-import javax.swing.JButton;
 import javax.swing.JTable;
-import javax.swing.table.TableCellRenderer;
 
-public class RedButtonRenderer implements TableCellRenderer {
-    
-    private final JButton button;
-    private String btnText;
-    
+public class RedButtonRenderer extends ButtonRenderer {
+
     public RedButtonRenderer(String btnText) {
-        
-        this.button = new JButton();
-        this.button.setOpaque(true);
-        this.btnText = btnText;
+        super(btnText);
     }
 
     @Override
-    public Component getTableCellRendererComponent(JTable table, Object value,
-        boolean isSelected, boolean hasFocus, int row, int column) {
-        
+    public Component getTableCellRendererComponent(
+            JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+
         button.setText(btnText);
 
-        //Button select animation
-        button.setForeground(new Color(215,76,76));
-        button.setBackground(new Color(215,76,76));
+        // Button select animation
+        button.setForeground(new Color(215, 76, 76));
+        button.setBackground(new Color(215, 76, 76));
 
         return button;
     }
